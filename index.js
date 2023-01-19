21ab40c2ff0046002e2193e6e2300669fa0e30ad
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-19 11:14:37 +0300

    Mongoose Part 3

--- index.js
+++ index.js
@@ -15,11 +15,25 @@ connect.then((db) => {
     .then((dish) => {
         console.log(dish);
 
-        return Dishes.find({}).exec();
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test' }
+        },{ 
+            new: true 
+        }).exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+        dish.comments.push({
+            rating: 5,
+            comment: 'Sample comment',
+            author: 'John Apples'
+        });
 
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+        
         return Dishes.deleteMany({});
     })
     .then(() => {
